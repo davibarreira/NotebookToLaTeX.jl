60c7fb96f1f986210758e0ab11139f77eacb4bec
Davi Barreira <davisbarreira@gmail.com>
2021-10-22 13:27:19 -0300

    Test for jupyter

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ using Test
 @testset "NotebookToLatex.jl" begin
     # Write your tests here.
     path = "./project/"
+
     @testset "Creating Folders and Files" begin
         createfolders(path)
         @test isdir(path)
@@ -20,21 +21,38 @@ using Test
         @test isfile(path * "/main.tex")
     end
 
-    @testset "Parsing Pluto Notebook" begin
-        nb = extractnotebook("./notebooktest.jl")
-        outputs  = collectoutputs(nb, path)
-        println(outputs)
-        @test outputs[1] == (:nothing, "")
-        @test outputs[2] == (:plot, "notebooktest_figure1.png")
-        @test isfile(path * "/figures/notebooktest_figure1.png")
-        #= rm(path, recursive=true) =#
-    end
+    #= @testset "Parsing Pluto Notebook" begin =#
+    #=     nb = extractnotebook("./notebooktest.jl") =#
+        #=     outputs  = collectoutputs(nb, path) =#
+    #=     println(outputs) =#
+    #=     @test outputs[1] == (:nothing, "") =#
+    #=     @test outputs[2] == (:plot, "notebooktest_figure1.png") =#
+    #=     @test isfile(path * "/figures/notebooktest_figure1.png") =#
+    #= end =#
 
-    @testset "Pluto to Latex" begin
-        notebooktolatex("./notebooktest.jl", template=:mathbook,
-                        fontpath="/home/davibarreira/.local/share/fonts/Unknown Vendor/TrueType/JuliaMono/")
+    #= @testset "Pluto to Latex" begin =#
+        #= notebooktolatex("./notebooktest.jl", template=:mathbook, =#
+        #=                 fontpath="/home/davibarreira/.local/share/fonts/Unknown Vendor/TrueType/JuliaMono/") =#
+        #= notebooktolatex("./notebooktest.jl", template=:mathbook) =#
+        #= @test isfile("./build_latex/notebooks/") =#
+    #= end =#
+
+    @testset "Jupyter to Latex" begin
+        jupyterpath = "./jupyter/build_latex/"
+        notebooktolatex("./jupyter/jupyternotebook.ipynb", jupyterpath, template=:mathbook)
+        @test isfile(jupyterpath * "main.tex")
+        @test isfile(jupyterpath * "preface.tex")
+        @test isfile(jupyterpath * "frontmatter/copyright.tex")
+        @test isfile(jupyterpath * "frontmatter/titlepage.tex")
+        @test isfile(jupyterpath * "julia_font.tex")
+        @test isfile(jupyterpath * "julia_listings.tex")
+        @test isfile(jupyterpath * "julia_listings_unicode.tex")
+        @test isfile(jupyterpath * "/fonts/JuliaMono_Bold.ttf")
+        @test isfile(jupyterpath * "/fonts/JuliaMono_Medium.ttf")
+        @test isfile(jupyterpath * "/fonts/JuliaMono_Regular.ttf")
+        notebooktolatex("./jupyter/jupyternotebook.ipynb", jupyterpath, template=:book)
     end
 
     rm(path, recursive=true)
-    rm("./build_latex/", recursive=true)
+    #= rm("./build_latex/", recursive=true) =#
 end
