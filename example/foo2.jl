61e78bacf381533c15399c4a72813a0ff99eaea8
Davi Barreira <davisbarreira@gmail.com>
2021-10-22 19:27:15 -0300

    Adjusted the markdown checker for Pluto notebooks in the case the user does md+quote instead of the triple quote. Also, fixed error for collecting the source code in the cells. In the future, will change to use Plutos internal functionality

---
+++ example/foo2.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 33012b19-0f0b-4943-aa99-e4a65a1de0f0
+md"Hello"
+
+# ╔═╡ e9d8b87c-f192-4ddc-8068-f25d132ad75f
+	md"""
+Oka
+"""
+
+# ╔═╡ f98090e8-23b4-44c1-99e3-da43571814bb
+
+
+# ╔═╡ 43e5dfe4-90b2-4f7b-b467-3107b78520f4
+x = 1 + 1
+
+# ╔═╡ 036fdfbc-94c9-42e2-be5e-54e8c958ce65
+
+
+# ╔═╡ Cell order:
+# ╠═33012b19-0f0b-4943-aa99-e4a65a1de0f0
+# ╠═e9d8b87c-f192-4ddc-8068-f25d132ad75f
+# ╠═f98090e8-23b4-44c1-99e3-da43571814bb
+# ╠═43e5dfe4-90b2-4f7b-b467-3107b78520f4
+# ╠═036fdfbc-94c9-42e2-be5e-54e8c958ce65
